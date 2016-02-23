bdfd73d6d41f4d0a8968f4aed1b8cbf54700028f
594904292 <594904292@qq.com>
2016-02-23 16:49:36 +0800

    找回密码

--- ResetPassViewController.swift
+++ ResetPassViewController.swift
@@ -7,12 +7,77 @@
 //
 
 import UIKit
-
+import Alamofire
 class ResetPassViewController: UIViewController {
 
+    var telphone:String="";
+    
+    @IBOutlet weak var pass1: UITextField!
+    
+    @IBOutlet weak var pass2: UITextField!
+    
+     var alertView:UIAlertView?
+    @IBAction func resetpass(sender: UIButton) {
+        if(pass1.text != pass2.text)
+        {
+            
+            self.alertView = UIAlertView()
+            self.alertView!.title = "提示"
+            self.alertView!.message = "电话不能为空"
+            self.alertView!.addButtonWithTitle("关闭")
+            NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+            self.alertView!.show()
+            return;
+
+        }
+        
+        let  dic:Dictionary<String,String> = ["_telphone" : telphone,"_password" : pass1.text!]
+        Alamofire.request(.POST, "http://www.bbxiaoqu.com/resetpass.php", parameters: dic)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                print(response.result.value)
+                if let ret = response.result.value  {
+                    // print("JSON: \(JSON)")
+                    if String(ret)=="1"
+                    {
+                        self.alertView = UIAlertView()
+                        self.alertView!.title = "提示"
+                        self.alertView!.message = "重设成功"
+                        self.alertView!.addButtonWithTitle("关闭")
+                        NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+                        self.alertView!.show()
+                        
+                        let sb = UIStoryboard(name:"Main", bundle: nil)
+                        let vc = sb.instantiateViewControllerWithIdentifier("loginController") as! LoginViewController
+                        self.presentViewController(vc, animated: true, completion: nil)
+
+                        
+                    } else
+                    {
+                        self.alertView = UIAlertView()
+                        self.alertView!.title = "提示"
+                        self.alertView!.message = "重设失败"
+                        self.alertView!.addButtonWithTitle("关闭")
+                        NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+                        self.alertView!.show()
+                    }
+                }
+        }
+
+    }
+    
+    
+    func dismiss(timer:NSTimer){
+        alertView!.dismissWithClickedButtonIndex(0, animated:true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title="重设密码"
         // Do any additional setup after loading the view.
     }
 
--- SearchPassViewController.swift
+++ SearchPassViewController.swift
@@ -7,13 +7,104 @@
 //
 
 import UIKit
-
+import Alamofire
 class SearchPassViewController: UIViewController {
 
+    @IBOutlet weak var telphone: UITextField!
+   
+    @IBOutlet weak var authcode: UITextField!
+    
+    @IBOutlet weak var nextbtn: UIButton!
+    var lastauthcode:String = "9811";
+    @IBAction func GetAuthcode(sender: UIButton) {
+        if(self.telphone.text!.characters.count==0)
+        {
+            self.alertView = UIAlertView()
+            self.alertView!.title = "提示"
+            self.alertView!.message = "电话不能为空"
+            self.alertView!.addButtonWithTitle("关闭")
+            NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+            self.alertView!.show()
+            return;
+        }
+
+        let tel:String = self.telphone.text as String!
+        let  dic:Dictionary<String,String> = ["_telphone" : tel]
+        Alamofire.request(.POST, "http://www.bbxiaoqu.com/getauthcode.php?show=true", parameters: dic)
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                print(response.result.value)
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                    self.authcode.text=String(JSON as! NSNumber);
+                    self.lastauthcode=String(JSON as! NSNumber);
+                }
+        }
+
+    }
+    
+    @IBAction func authcodechange(sender: UITextField) {
+        if(self.lastauthcode == sender.text)
+        {
+        
+            self.nextbtn.enabled = true
+
+        }else
+        {
+            self.nextbtn.enabled = false
+
+        }
+    
+    }
+    var alertView:UIAlertView?
+
+    @IBAction func NextAction(sender: UIButton) {
+        
+        
+        
+        if(self.telphone.text!.characters.count==0)
+        {
+            self.alertView = UIAlertView()
+            self.alertView!.title = "提示"
+            self.alertView!.message = "电话不能为空"
+            self.alertView!.addButtonWithTitle("关闭")
+            NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+            self.alertView!.show()
+            return;
+        }
+        if(self.authcode.text!.characters.count==0)
+        {
+            self.alertView = UIAlertView()
+            self.alertView!.title = "提示"
+            self.alertView!.message = "认证码不能为空"
+            self.alertView!.addButtonWithTitle("关闭")
+            NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"dismiss:", userInfo:self.alertView!, repeats:false)
+            self.alertView!.show()
+            return;
+        }
+        let sb = UIStoryboard(name:"Main", bundle: nil)
+        let vc = sb.instantiateViewControllerWithIdentifier("resetpassController") as! ResetPassViewController
+        vc.telphone = telphone.text!
+        //创建导航控制器
+        let nvc=UINavigationController(rootViewController:vc);
+        //设置根视图
+        self.view.window!.rootViewController=nvc;
+
+    }
+    
+    func dismiss(timer:NSTimer){
+        alertView!.dismissWithClickedButtonIndex(0, animated:true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.navigationItem.title="忘记密码"
         self.title="忘记密码"
+        nextbtn.enabled=false
         // Do any additional setup after loading the view.
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.Done, target: self, action: "backClick")
         
