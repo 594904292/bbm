d0dc44e7d0e3cfda7ffab7f40b59f9e11c4e78f0
songgc <594904292@qq.com>
2016-04-01 16:53:38 +0800

    评分

---
+++ bbm/ItemEvaluate.swift
@@ -0,0 +1,31 @@
+//
+//  Evaluate.swift
+//  bbm
+//
+//  Created by songgc on 16/3/31.
+//  Copyright © 2016年 sprin. All rights reserved.
+//
+
+import UIKit
+
+class ItemEvaluate: NSObject {
+    var id:String = ""
+    var guid:String = ""
+    var infouser:String = ""
+    var userid:String = ""
+    var score:String = ""
+    var evalute:String = ""
+    var addtime:String = ""
+    
+    init(id:String,guid:String,infouser:String,userid:String,score:String,evalute:String,addtime:String)
+    {
+        self.id = id
+        self.guid = guid
+        self.infouser = infouser
+        self.userid = userid
+        self.score = score
+        self.evalute = evalute
+        self.addtime = addtime
+    }
+    
+}
