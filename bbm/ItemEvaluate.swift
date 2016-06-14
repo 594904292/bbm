bcab3ec63c12594f0c29440dcca9df84e4582e1c
songgc <594904292@qq.com>
2016-06-14 11:59:17 +0800

    隐藏电话号码

--- bbm/ItemEvaluate.swift
+++ bbm/ItemEvaluate.swift
@@ -12,16 +12,18 @@ class ItemEvaluate: NSObject {
     var id:String = ""
     var guid:String = ""
     var infouser:String = ""
+    var username:String = ""
     var userid:String = ""
     var score:String = ""
     var evalute:String = ""
     var addtime:String = ""
     
-    init(id:String,guid:String,infouser:String,userid:String,score:String,evalute:String,addtime:String)
+    init(id:String,guid:String,infouser:String,username:String,userid:String,score:String,evalute:String,addtime:String)
     {
         self.id = id
         self.guid = guid
         self.infouser = infouser
+        self.username=username
         self.userid = userid
         self.score = score
         self.evalute = evalute
